07187e0213154db2eba091f03f139040b00cb65c
sullygregory <sullygregory@vt.edu>
2022-08-09 22:07:20 -0400

    Change implementation to use squares of color instead of ones of variable size
    This was done to make it easier to crop source images, at the cost of potentially losing a few pixels on the input image.

--- mosaic.py
+++ mosaic.py
@@ -1,14 +1,13 @@
 from PIL import Image
 import math
 
-N_ROWS = 100
-N_COLUMNS = N_ROWS
-image = Image.open("pictures/lamelo.png")
+WIDTH = 10
+image = Image.open('pictures/lamelo.png')
 
 def main():
     # Get even square dimensions to be replaced and generate output image
     squares = getSquares()
-    newImg = Image.new("RGB", (image.width, image.height))
+    newImg = Image.new('RGB', (image.width, image.height))
 
     # Replace every square in new picture with average color of the corresponding square in the original image
     for square in squares:
@@ -17,26 +16,20 @@ def main():
         replaceSquare(newImg, square, avgColor)
 
     newImg.show()
+    image.close()
         
 # Divides up image into squares relatively the same size, and store the coords of each square into a list
 def getSquares():
-    # Get average width and height of each square
-    dWidth = image.width / N_COLUMNS
-    dHeight = image.height / N_ROWS
-
-    # Generate each square coord
+    # Generate each square's coordinates
     squares = []
-    for rows in range(N_ROWS):
-        # Calc top and bottom coordinate, use ceiling to avoid a decimal and ensure every pixel is used
-        top = math.ceil(rows * dHeight)
-        bottom = math.ceil(top + dHeight)
+    y = WIDTH
+    while y <= image.height:
+        x = WIDTH
+        while x <= image.width:
+            squares.append((x - WIDTH, y - WIDTH, x, y))
+            x += WIDTH
 
-        # Calc left and right coordinate
-        for columns in range(N_COLUMNS):
-            left = math.ceil(columns * dWidth)
-            right = math.ceil(left + dWidth)
-
-            squares.append((left, top, right, bottom))
+        y += WIDTH
 
     return squares
 
@@ -62,9 +55,10 @@ def getAvgColor(img):
 def replaceSquare(img, square, color):
     width = square[2] - square[0]
     height = square[3] - square[1]
-    newImg = Image.new("RGB", (width, height), color)
+    newImg = Image.new('RGB', (width, height), color)
 
     img.paste(newImg, square)
 
 if __name__ == '__main__':
     main()
+    image.close()
