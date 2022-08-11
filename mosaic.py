752d22456b69901beb6efe77aaa4e11661d70b6e
sullygregory <sullygregory@vt.edu>
2022-08-11 10:07:44 -0400

    Change function and variable names to match conventions

--- mosaic.py
+++ mosaic.py
@@ -5,50 +5,49 @@ import math, glob
 # Get target image
 im = Image.open('pictures/elk.png')
 im = ImageOps.exif_transpose(im) # Rotate if need be
-targetImg = im.convert('RGB') # Convert to RGB img
+target_img = im.convert('RGB') # Convert to RGB im
 
 N_ROWS = 144
-SQUARE_WIDTH = targetImg.height // N_ROWS
-N_COLUMNS = targetImg.width // SQUARE_WIDTH
+SQUARE_WIDTH = target_img.height // N_ROWS
+N_COLUMNS = target_img.width // SQUARE_WIDTH
 
 def main():
     # Get source images
-    srcImgs = []
+    src_imgs = []
     for file in glob.glob('pictures/sized-images/*'):
-        srcImgs.append(Image.open(file))
+        src_imgs.append(Image.open(file))
 
     # Get even square coords to be replaced
-    targetSquares = getSquares(targetImg, SQUARE_WIDTH)
+    target_squares = get_squares(target_img, SQUARE_WIDTH)
 
     # Get source image width and use that to calculate dimensions of output image
-    srcImagesWidth = srcImgs[0].width
-    dim = (N_COLUMNS * srcImagesWidth, N_ROWS * srcImagesWidth)
+    src_imgs_width = src_imgs[0].width
+    dim = (N_COLUMNS * src_imgs_width, N_ROWS * src_imgs_width)
 
     # Create output image
-    outputImg = Image.new('RGB', dim)
-    outputSquares = getSquares(outputImg, srcImagesWidth)
+    output_img = Image.new('RGB', dim)
+    output_squares = get_squares(output_img, src_imgs_width)
 
     # Create list of average colors for each source image
-    avgSrcColors = []
-    for img in srcImgs:
-        avgSrcColors.append(calcAvgColor(img))
+    avg_src_colors = []
+    for img in src_imgs:
+        avg_src_colors.append(calc_avg_color(img))
 
     # Replace every square in new picture with best matching image from source images
-    for sqIndex in range(len(targetSquares)):
-        crop = targetImg.crop(targetSquares[sqIndex])
-        matchingImg = srcImgs[findIndexClosestColor(crop, avgSrcColors)]
-        outputImg.paste(matchingImg, outputSquares[sqIndex])
+    for sq_index in range(len(target_squares)):
+        crop = target_img.crop(target_squares[sq_index])
+        matching_img = src_imgs[find_index_closest_color(crop, avg_src_colors)]
+        output_img.paste(matching_img, output_squares[sq_index])
 
-    outputImg.show()
-    outputImg.save('pictures/output.jpg')
+    output_img.show()
+    output_img.save('pictures/output.jpg')
 
-    # Close all images
-    targetImg.close()
-    for img in srcImgs:
+    # Close all source images
+    for img in src_imgs:
         img.close()
         
 # Divides up image into squares of the same size, and store the coords of each square into a list
-def getSquares(img, width):
+def get_squares(img, width):
     # Generate each square's coordinates
     squares = []
     for r in range(N_ROWS):
@@ -60,10 +59,10 @@ def getSquares(img, width):
     return squares
 
 # Gets average color of a picture
-def calcAvgColor(img):
+def calc_avg_color(img):
     # Generate list of pixels in picture
     pixels = list(img.getdata())
-    nPixels = len(pixels)
+    n_pixels = len(pixels)
 
     r = 0
     g = 0
@@ -75,30 +74,30 @@ def calcAvgColor(img):
         b += pixel[2]
     
     # Divide each by num of pixels to get avg and return
-    return (r // nPixels, g // nPixels, b // nPixels)
+    return (r // n_pixels, g // n_pixels, b // n_pixels)
 
 # Get difference between 2 colors using distance formula
-def calcDiffColor(colorA, colorB):
-    totalSquareDiff = 0
+def calc_diff_color(colorA, colorB):
+    total_square_diff = 0
     for a, b in zip(colorA, colorB):
-        totalSquareDiff += (b - a) ** 2
+        total_square_diff += (b - a) ** 2
 
-    return math.sqrt(totalSquareDiff)
+    return math.sqrt(total_square_diff)
 
 # Find index of color in list closest in color to inputted image
-def findIndexClosestColor(img, colors):
-    targetColor = calcAvgColor(img)
+def find_index_closest_color(img, colors):
+    target_color = calc_avg_color(img)
 
     # Find closest color by tracking the min difference in color found while looping through array
-    closestColorIndex = 0
-    min = calcDiffColor(targetColor, colors[0])
+    closest_clr_index = 0
+    min = calc_diff_color(target_color, colors[0])
     for i in range(1, len(colors)): # Starts at 1 to skip 0 which is the preset
-        diffColor = calcDiffColor(targetColor, colors[i])
-        if diffColor < min:
-            closestColorIndex = i
-            min = diffColor
+        diff_color = calc_diff_color(target_color, colors[i])
+        if diff_color < min:
+            closest_clr_index = i
+            min = diff_color
 
-    return closestColorIndex
+    return closest_clr_index
 
 if __name__ == '__main__':
     main()
